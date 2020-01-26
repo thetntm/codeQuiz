f652a5febfbf3f13d2d08230a0b7359498403ddd
thetntm <michael.becker.123@gmail.com>
2020-01-26 00:31:07 -0800

    added more things so you can actually take a full quiz now

--- questions.js
+++ questions.js
@@ -21,4 +21,51 @@ var testQuestion = {
 }
 //This method of using objects as answers will allow for multiple correct answers.
 
-var questions = [testQuestion];
+var questions = 
+[
+    //question 1:
+    {
+        prompt:"What is the correct answer?",
+        answers:[
+            {
+                statement:"this one",
+                isCorrect:true
+            },
+            {
+                statement:"that one",
+                isCorrect:false
+            },
+            {
+                statement:"um",
+                isCorrect:false
+            },
+            {
+                statement:"I don't know",
+                isCorrect:false
+            }
+        ]
+    },
+    //question 2:
+    {
+        prompt:"Ok, how about this one?",
+        answers:[
+            {
+                statement:"Don't click me!",
+                isCorrect:false
+            },
+            {
+                statement:"Click me!",
+                isCorrect:true
+            },
+            {
+                statement:"Or click me!",
+                isCorrect:true
+            },
+            {
+                statement:"But not me!",
+                isCorrect:false
+            }
+        ]
+    }
+
+]
--- quiz.js
+++ quiz.js
@@ -24,18 +24,66 @@ var answerButtons = [$("#button0"),$("#button1"),$("#button2"),$("#button3")];
 
 var questionPrompt = $("#questionPrompt");
 
+var timer = $("#timer");
+
+var score = $("#score");
+
+var timeBonusMessage = $("#timeBonus");
+
+var endCondition=$("#endingCondition");
+
 //Variables
 var question_index = 0;
 
 var current_score = 0;
 
+var remaining_time = 0;
+
+var time_bonus = true;
+
+var time_bonus_window = 1000;
+
 var currentQuestion = questions[0];
 
 var currentAnswers = questions[0].answers;
 
+//Intervals and Timeouts
+
+var questionTimeout = undefined;
+
+var transitionTimeout = undefined;
+
+var timerInterval = undefined;
+
+//Game Variables
+
+var correctAnswerPoints = 15;
+
+var timeBonusPoints = 10;
+
+var wrongAnswerPenalty = 10;
+
+var completionBonus = 10;
+
+
 //functions
+
+//Update functions
 function updateQuestion()
 {
+    if (!(question_index < questions.length)) {
+        clearInterval(timerInterval);
+        clearTimeout(questionTimeout);
+        clearTimeout(transitionTimeout);
+
+        current_score += completionBonus;
+        updateScore();
+
+        endCondition.text("You made it to the end! +" + completionBonus + " bonus points!");
+        setQuizView(displayHighScoreInput);
+
+        return false;
+    }
     //set up shortcuts
     currentQuestion = questions[question_index];
     currentAnswers = questions[question_index].answers;
@@ -46,7 +94,14 @@ function updateQuestion()
         const b = answerButtons[i];
         b.text(currentAnswers[i].statement);
     }
+    return true;
 }
+
+function updateTimer() {timer.text(remaining_time);}
+
+function updateScore() {score.text(current_score);}
+
+//useful functions
 
 function setQuizView(view)
 {
@@ -65,27 +120,100 @@ function setQuizView(view)
     view.css("display","inline");
 }
 
+//event/interval/timeout functions
+
 function answerClicked(event)
 {
     let index = parseInt(this.id.replace("button",""));
     console.log(index);
     if (currentAnswers[index].isCorrect) {
+        if (time_bonus) {
+            current_score += 15;
+            timeBonusMessage.text("Time Bonus! +" + timeBonusPoints + " points!")
+        } else
+        {
+            timeBonusMessage.text("");
+        }
         setQuizView(displayCorrect);
+
+        current_score += 15;
+        updateScore();
     } else
     {
+        remaining_time -= wrongAnswerPenalty;
+        updateTimer();
         setQuizView(displayIncorrect);
     }
-    debugger;
+
+    //Pause the timer while we show the result
+    clearInterval(timerInterval)
+
+    //prepare to load up the next question
+    setTimeout(nextQuestion,1000);
 }
+
+function countDown()
+{
+    remaining_time -= 1;
+    updateTimer();
+    if (remaining_time<=0)
+    {
+        remaining_time = 0;
+
+        //Code for end of the game.
+        clearInterval(timerInterval);
+        clearTimeout(questionTimeout);
+        clearTimeout(transitionTimeout);
+
+        endCondition.text("Time ran out! Better luck next time!");
+
+        setQuizView(displayHighScoreInput);
+    }
+}
+
+function nextQuestion()
+{
+    question_index++;
+
+    time_bonus = true;
+
+    questionTimeout = setTimeout(removeTimeBonus,time_bonus_window);
+    timerInterval = setInterval(countDown,1000);
+
+    if (updateQuestion())
+    {
+        setQuizView(displayQuestion);
+    }
+}
+
+function removeTimeBonus() {time_bonus = false;}
 
 
 // Event Assignment
 
 startButton.click(
     function() {
+        //set the timer
+        for (let i = 0; i < questions.length; i++) {
+            remaining_time += 15;
+        }
+        
+
+        //reset variables
         question_index = 0;
         current_score = 0;
+        time_bonus = true;
+
+        //updates
         updateQuestion();
+        updateTimer();
+        updateScore();
+        
+        //setup intervals
+        questionTimeout = setTimeout(removeTimeBonus,time_bonus_window);
+        timerInterval = setInterval(countDown,1000);
+
+        //show the question
         setQuizView(displayQuestion);
     }
 )
